1861b634a73e830a2f67b0813b0d15b3062f315e
Abdur Rahman <abdurrah2511@gmail.com>
2026-05-06 10:15:43 +0400

    server update

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/products", productRoutes);
 
-app.use("/images", express.static(path.join(__dirname,"images")));
+app.use("/images", express.static(path.join(__dirname,"defaults")));
 
 app.use("/api/orders", orderRoutes);
